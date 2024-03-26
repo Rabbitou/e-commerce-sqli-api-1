ebca929d07537d7fe8bc6e5d73e2a4d927cfddd2
Rabbitou <48840164+Rabbitou@users.noreply.github.com>
2024-03-26 14:05:37 +0000

    feat:add index and folders

--- dist/index.js
+++ dist/index.js
@@ -3,16 +3,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const categories_json_1 = __importDefault(require("./data/categories.json"));
+const products_json_1 = __importDefault(require("./data/products.json"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8080;
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
+const port = process.env.PORT || 8000;
+const productsRouter = express_1.default.Router();
+const categoriesRouter = express_1.default.Router();
+app.use((0, cors_1.default)());
+app.use('/products', productsRouter);
+app.use('/categories', categoriesRouter);
+productsRouter.get('/', (req, res) => {
+    res.send(products_json_1.default);
 });
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
+productsRouter.get('/:id?', (req, res) => {
+    const { id } = req.params;
+    res.send(products_json_1.default.find((p) => p.id === parseInt(id)));
+});
+productsRouter.get('/cat/:id?', (req, res) => {
+    const { id } = req.params;
+    res.send(products_json_1.default.filter((p) => p.category_id === parseInt(id)));
+});
+categoriesRouter.get('/', (req, res) => {
+    res.send(categories_json_1.default);
+});
+categoriesRouter.get('/:id?', (req, res) => {
+    const { id } = req.params;
+    res.send(categories_json_1.default.find((c) => c.id === parseInt(id)));
+});
+categoriesRouter.get('/name/:name?', (req, res) => {
+    const { name } = req.params;
+    console.log(req.params);
+    console.log("name: ", name);
+    res.send(categories_json_1.default.find((c) => c.category_name === name.toUpperCase()));
 });
 app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
+    console.log(`Server ready on port ${port}.`);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,52 @@
-import express, { Request, Response } from 'express'
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import categories from './data/categories.json';
+import products from './data/products.json';
+import { Product } from 'product';
 
-const app = express()
-const port = process.env.PORT || 8080
+const app: Application = express();
+const port = process.env.PORT || 8000;
+const productsRouter = express.Router();
+const categoriesRouter = express.Router();
 
-app.get('/', (_req: Request, res: Response) => {
-  return res.send('Express Typescript on Vercel')
+app.use(cors())
+app.use('/products', productsRouter)
+app.use('/categories',categoriesRouter)
+
+productsRouter.get('/', (req:Request,res:Response) => {
+    res.send(products)
+});
+
+productsRouter.get('/:id?', (req:Request,res:Response) => {
+    const {id} = req.params;
+    res.send(products.find((p: Product) => p.id === parseInt(id)))
+});
+
+productsRouter.get('/cat/:id?', (req:Request,res:Response) => {
+    const {id} = req.params;
+    res.send(products.filter((p: Product) => p.category_id === parseInt(id)))
+});
+
+categoriesRouter.get('/', (req:Request,res:Response) => {
+    
+    
+    res.send(categories)
+});
+
+categoriesRouter.get('/:id?', (req:Request,res:Response) => {
+    const {id} = req.params
+    res.send(categories.find((c: { id: number; }) => c.id === parseInt(id)))
 })
 
-app.get('/ping', (_req: Request, res: Response) => {
-  return res.send('pong 🏓')
+categoriesRouter.get('/name/:name?', (req:Request,res:Response) => {
+    const {name} = req.params;
+    console.log(req.params);
+    console.log("name: ",name);
+    
+    res.send(categories.find((c: { category_name: string; }) => c.category_name === name.toUpperCase()))
 })
+
 
 app.listen(port, () => {
-  return console.log(`Server is listening on ${port}`)
-})
+    console.log(`Server ready on port ${port}.`)
+});
